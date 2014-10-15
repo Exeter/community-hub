03562017cbeda0df3721cde2423390cb79a7a3dc
Weihang Fan <weihangf@gmail.com>
2014-10-15 16:36:18 -0700

    added test login

--- routes.py
+++ routes.py
@@ -10,22 +10,10 @@ config['webapp2_extras.sessions'] = {
 }
 
 application = webapp2.WSGIApplication([
-    Route('/register', 'handlers.account.RegisterHandler'),
+#    Route('/register', 'handlers.account.RegisterHandler'),
     Route('/login', 'handlers.account.LoginHandler'),
     Route('/logout', 'handlers.account.LogoutHandler'),
-    Route('/forgot', 'handlers.account.ForgotPasswordHandler'),
-    Route('/create_team', 'handlers.team.CreateTeamHandler'),
-    Route('/add_legacy_team', 'handlers.team.AddLegacyTeamHandler'),
-    Route('/list_all', 'handlers.team.ListAllHandler'),
-    Route('/edit_info', 'handlers.team.EditInfoHandler'),
-    Route('/list', 'handlers.team.ListHandler'),
-    Route('/admin_list', 'handlers.team.AdminListHandler'),
-    Route('/reset/<user_id:\d+>-<signup_token:.+>', 'handlers.template.ResetPasswordHandler', name='reset'),
-    Route('/reset', 'handlers.account.SetPasswordHandler'),
-    Route('/cleanup', 'handlers.account.CleanupHandler'),
-    Route('/send_emails', 'handlers.team.SendEmailHandler'),
-    Route('/add_legacy', 'handlers.account.AddLegacyUserHandler'),
-    Route('/grade', 'handlers.team.GradeHandler'),
-    Route('/check', 'handlers.team.CheckHandler')
+    Route('/main_page', 'handlers.account.MainPage'),
+#    Route('/forgot', 'handlers.account.ForgotPasswordHandler'),
 ], debug=True, config=config)
 
